7819ea907aa28ed8f98ade7df4fc14b04a4c5488
Dimuthu Lakmal <lakmalepp@gmail.com>
2023-10-08 20:35:04 +0530

    resolve

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-<<<<<<< HEAD
 import { NotFound } from "./pages/NotFound";
-=======
-import Bloglist from "./pages/Bloglist";
-import BlogDetails from "./pages/BlogDetails";
->>>>>>> master
 import "./App.scss";
 
 function App() {
@@ -16,14 +11,9 @@ function App() {
         <Route path="/" exact>
           <Homepage />
         </Route>
-<<<<<<< HEAD
         {/* <Route path="/blogs" component={Bloglist} exact />
         <Route path="/blogs/blog-details/:id/:title" component={BlogDetails} /> */}
         <Route component={NotFound} />
-=======
-        <Route path="/blogs" component={Bloglist} exact />
-        <Route path="/blogs/blog-details/:id/:title" component={BlogDetails} />
->>>>>>> master
       </Switch>
     </HashRouter>
   );
